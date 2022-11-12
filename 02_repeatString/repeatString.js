9f681112bb28fb9d7c29c1e7a5cce047ddb3c12c
nsoimaru <nsoimaru@gmail.com>
2022-11-12 21:52:03 +0200

    Finish first batch of js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let text = "";
+    for (let i = 1; i <= num; i++) {
+        text = string + text;
+    }
+    if(num < 0) {
+        return "ERROR";
+    }
+    return text;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(text) {
+let reverseText = text.split("").reverse().join("");
+// let reverseArrText = splitArrText.reverse();
+// let revString = reverseArrText.join()
 
+return reverseText;
 };
 
 // Do not edit below this line
