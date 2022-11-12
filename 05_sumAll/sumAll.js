ee98a409b9acbb3d3d86fc6aa4536d6a61efaad5
nsoimaru <nsoimaru@gmail.com>
2022-11-12 22:47:44 +0200

    Finish second batch of exercises.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let count = num1;
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        let count = num2;
+        while(count <= num1) {
+            sum = sum + count;
+            count++;
+        }
+    } else {
+        let count = num1;
+        while(count <= num2) {
+            sum = sum + count;
+            count++;
+        }
+    }
+
+
+    return sum;
 };
 
 // Do not edit below this line
